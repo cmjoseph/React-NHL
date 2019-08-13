e0c87d4886727d320ad88623307c8322aa2829c3
Claude Marc Joseph <cmjoseph@parkour3.com>
2019-08-13 16:37:57 -0400

    folder structure

---
+++ src/components/Nav.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Nav extends Component {
+  render() {
+    return (
+        <div>
+          <h2>Welcome to React Router Tutorial</h2>
+        </div>
+    );
+  }
+}
+
+export default Nav;
