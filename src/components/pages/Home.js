70240b560403d9d7838e0195a71c4799c37a4769
Claude Marc Joseph <cmjoseph@parkour3.com>
2019-08-19 16:52:44 -0400

    Slider home page test

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,13 +1,60 @@
 import React from 'react';
 import Slider from '../slider/Slider';
 
-const Home = () => {
-    return (
-        <div className="home__template">
-            <h1>Home</h1>
-            <Slider></Slider>
-        </div>
-    );
+class Home extends React.Component {
+
+    constructor(){
+        super();
+        
+        this.images = [
+            'https://picsum.photos/400/300/?image=926',
+            'https://picsum.photos/400/300/?image=925',
+            'https://picsum.photos/400/300/?image=924',
+            'https://picsum.photos/400/300/?image=923',
+            'https://picsum.photos/400/300/?image=922',
+            'https://picsum.photos/400/300/?image=921',
+        ];
+        
+        const hasOwn = {}.hasOwnProperty;
+
+        const classNames = (...args) => {
+            var classes = [];
+
+            for (let i = 0; i < args.length; i++) {
+                let arg = args[i];
+                if (!arg) continue;
+
+                let argType = typeof arg;
+
+                if (argType === 'string' || argType === 'number') {
+                    classes.push(arg);
+                } else if (Array.isArray(arg) && arg.length) {
+                    let inner = classNames.apply(null, arg);
+                    if (inner) {
+                        classes.push(inner);
+                    }
+                } else if (argType === 'object') {
+                    for (let key in arg) {
+                        if (Object.hasOwnProperty.call(arg, key) && arg[key]) {
+                            classes.push(key);
+                        }
+                    }
+                }
+            }
+
+            return classes.join(' ');
+        }
+    }
+
+
+    render(){
+        return (
+            <div className="home__template">
+                <h1>Home</h1>
+                <Slider images={this.images} />
+            </div>
+        );
+    }
 }
 
 export default Home;
